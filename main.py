320fe17aaa12df89d34918337c02df6bd65a3888
axiewq <kocmos2048@gmail.com>
2024-03-25 14:20:43 +0700

    Обновил, убрал ошибки и добавил декоратор property

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
+
 with open('data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
 
 
 class Category:
@@ -9,25 +9,21 @@ class Category:
     total_unique_products = 0
 
     def __init__(self, name, description, products):
+        self.__products = products.copy()  # Создаем копию списка продуктов
         self.name = name
         self.description = description
-        self.products = []
-
-        for product in products:
-            self.products.append(product)
 
         Category.total_categories += 1
-        Category.total_unique_products += len(self.products)
+        Category.total_unique_products += len(products)
 
     def add_product(self, product):
-        self.products.append(product)
+        self.__products.append(product)
         Category.total_unique_products += 1
 
     def get_product_info(self):
-        for product in self.products:
+        for product in self.__products:
             info = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
             print(info)
-
 
 
 class Product:
@@ -46,23 +42,19 @@ class Product:
         product = cls(name, description, price, quantity)
         return product
 
+    @property
+    def price(self):
+        return self.price
 
-    def set_price(self,price):
-        if price < 0 or type(price) not in (int,float):
+    price.setter
+    def price(self, price):
+        if price < 0 or type(price) not in (int, float):
             print("Введена некорректная цена")
         else:
             self.price = price
-
-    def get_price(self):
-        return self.price
-
 
 
 categories = []
 for entry in data:
     category = Category(entry['name'], entry['description'], entry['products'])
     categories.append(category)
-
-
-
-
